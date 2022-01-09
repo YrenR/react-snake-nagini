be20fc75a86b78c6d6071cedcc27e9bae0cca535
Yren R <yren.rivero@gmail.com>
2022-01-09 15:05:58 +0100

    feat: add method for verification arrays empty

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,3 +1,5 @@
 export const getRandomInt = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
+
+export const isEmpty = (arr: Array<any>): boolean => arr.length === 0;
