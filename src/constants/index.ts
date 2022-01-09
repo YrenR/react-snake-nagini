a526e5dbda27cf708109a55ea8fa2be347e2360d
Yren R <yren.rivero@gmail.com>
2022-01-09 15:09:31 +0100

    feat: remove const color for food

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,16 +9,3 @@ export const scorePoints = {
   move: 1,
   eatFood: 25,
 };
-
-export const colorFood = [
-  "#fac89e",
-  "#e3e891",
-  "#c2fc99",
-  "#a3fcb3",
-  "#92e8d5",
-  "#96c8f2",
-  "#ada8ff",
-  "#ce94f7",
-  "#ed94dd",
-  "#fea8bb",
-];
